e5bb584fc5b3feee68e5fd8613ed47ed00816755
igor04 <igor.s04g@gmail.com>
2015-03-15 19:32:33 +0200

    Removed unnecessary comments

--- if-case.hs
+++ if-case.hs
@@ -1,15 +1,6 @@
--- IF ..  THEN .. ELSE
 big x y = if x < y
           then y
           else x
-
-
--- CASE
-someCase x = "The argument is: " ++ argument
-  where argument =
-          case x of
-            0 -> "null"
-            xs -> show x
 
 
 theSameCase x = "The argument is: " ++
@@ -18,3 +9,8 @@ theSameCase x = "The argument is: " ++
     1 -> "one"
     2 -> "two"
     xs -> show x
+
+someCase x = "The argument is: " ++ argument
+  where argument = case x of
+                     0 -> "null"
+                     xs -> show x
